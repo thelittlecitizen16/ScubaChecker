95fbb183353f4e992156da37e67713765a983ae1
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-30 17:44:45 +0300

    add serlizer class

---
+++ ScubaChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScubaChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ScubaChecker/Serializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MamasChecker.Abstract;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ScubaChecker
+{
+    public class Serializer<T> : ISerializer<T>
+    {
+        Stream ISerializer<T>.Serialize(T[] testsToSerialize)
+        {
+            XmlSerializer writer = new XmlSerializer(testsToSerialize.GetType());
+            StreamWriter fileWrite = new StreamWriter(@"C:\Users\thelittlecitizen16\source\repos\ScubaChecker\ScubaChecker\TestsXML.xml");
+            writer.Serialize(fileWrite, testsToSerialize);
+
+            return fileWrite.BaseStream;
+        }
+    }
+}
